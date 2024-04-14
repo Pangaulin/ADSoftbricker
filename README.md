c4c5bfdaef2576aac2d5dcd2c8d93e8915d467c5
Pangaulin <contact.pangaulin@gmail.com>
2024-04-14 12:22:31 +0200

    Add MIT license

--- README.md
+++ README.md
@@ -1,12 +1,41 @@
-I'm not responsible of any damage this software can make to your phone.
+# ADSoftbricker
 
-You need to have ADB installed on your computer, if you don't have it, you con follow this [this link](https://www.youtube.com/watch?v=26GI3z6tI3E).
-Check if your device have developer mode enabled AND ADB wireless debuging.
+This software is for educational purposes only, I am not responsible of any damage made on your phone. This software was tested on Windows 10 and Windows 11 with Python 3.12, on a Samsung A03
 
-After installing ADB, pair the device ; if you don't know how to do, you can follow [this link](https://android.gadgethacks.com/how-to/set-up-wireless-debugging-android-11-send-adb-commands-without-usb-cable-0302898/)
-Check if you have Python installed on your computer, else, install it.
+## How to start the program
 
-If you are on windows, just use the start.bat file to launch the script, else, start a command shell and write :
-`python main.py`
+In a command line, type :
 
-After this, follow the instructions !
+```bash
+  git clone https://github.com/Pangaulin/ADSoftbricker
+  cd ADSoftbricker
+  python main.py
+```
+Or : 
+- Download source code
+- Extract it with WinRAR, 7-Zip, on an other data compression and archiving software
+- Launch the **start.bat** file
+
+Then, enjoy !
+## How to use USB Debug
+After starting the software, you have two choices
+```bash
+USB Debug (1)
+Wireless Debug (2)
+```
+If you choose USB debug, check if it's activated on your phone, here is a [tutorial](https://developer.android.com/studio/debug/dev-options?hl=en#enable). 
+
+After this, plug the device to you computer, select 1, and enjoy !
+## How to use Wireless Debug
+
+After starting the software, you have two choices
+```bash
+USB Debug (1)
+Wireless Debug (2)
+```
+
+If you choose Wireless debug select 2, follow the same tutorial, but enable the "Use wireless debugging" option. If you have already paired your device, press "y", and write the IP adress, in this case "192.168.1.242", then the port, in this case "38157", and enjoy !
+
+![alt text](https://developer.android.com/static/studio/images/run/adb_wifi-wireless_debugging.png)
+
+If you have not paired your device before, press "n", select "Pair device with pairing code" on your phone. Then write the new IP adress on the software, the new port, then the six digit code. After it's successfully paired, leave the "Pair device" menu on your phone, write the new IP adress, in this case "192.168.1.242", then the port, in this case "38157", then enjoy ! 
--- modules/login.py
+++ modules/login.py
@@ -4,29 +4,67 @@ from config import adb_path
 
 def login(method):
     if method == 1:
-        print("Unsupported")
-        input("Please press ENTER to continue...")
-        exit()
+        print("Please check your device is connected in USB, and if USB debug is activated on your phone")
+        input("Press ENTER when it's done...")
     
     if method == 2:
         regex_ip = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
         regex_port = r'^([0-9]|[1-9][0-9]{1,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$'
 
         while True:
-            device_ip = input('Device IP (Generally as XXX.XXX.X.XX): ')
+            try:
+                is_paired = input("Is your device paired with ADB ? (y / n) : ")
+                is_paired = is_paired.lower()
+                if is_paired != "y" and is_paired != "n":
+                    print("Invalid input. Please enter a valid choice.")
+                else:
+                    match is_paired:
+                        case "y":
+                            is_paired = True
+                        case "n":
+                            is_paired = False
+                    break
+            except ValueError:
+                print("Invalid input. Please enter a valid choice.")
+
+        if is_paired == False:
+            while True:
+                device_pair_ip = input('Device pairing IP (Generally as XXX.XXX.X.XX): ')
+                if not re.match(regex_ip, device_ip):
+                    print("The IP adress isn't valid")
+                else:
+                    break
+            while True:
+                device_pair_port = input('Device pair port (Generally as XXXXX) : ')
+                if not re.match(regex_port, device_port):
+                    print("The port isn't a valid port")
+                else:
+                    break
+
+            pair_return = subprocess.run([adb_path, 'pair', f'{device_pair_ip}:{device_pair_port}'], shell=True, capture_output=True, text=True)
+            print(pair_return.stdout)
+
+        while True:
+            if is_paired == True:
+                device_ip = input('Device IP (Generally as XXX.XXX.X.XX): ')
+            else:
+                device_ip = input('Device IP (not the same as Device Pair IP) (Generally as XXX.XXX.X.XX): ')
             if not re.match(regex_ip, device_ip):
                 print("The IP adress isn't valid")
             else:
                 break
 
         while True:
-            device_port = input('Device port (Generally as XXXXX) : ')
+            if is_paired == True:
+                device_port = input('Device port (Generally as XXXXX) : ')
+            else:
+                device_port = input('Device port (not the same as Device Pair port) (Generally as XXXXX) : ')
             if not re.match(regex_port, device_port):
                    print("The port isn't a valid port")
             else:
                 break
 
-        connect_return = subprocess.run([adb_path, 'connect', f'{device_ip}:{device_port}'], shell=False, capture_output=True, text=True, check=False)
+        connect_return = subprocess.run([adb_path, 'connect', f'{device_ip}:{device_port}'], shell=False, capture_output=True, text=True)
         if "cannot connect" in connect_return.stdout:
             print("An error has occured while connecting to the device, please check the IP adress and the device port")
             input("Please press ENTER to continue...")
